3185c2f542cf3a0eb54599cee0dafcf5abe7d29d
Alex Phinizy <AlexPhinizy@gmail.com>
2013-09-11 16:32:42 -0400

    act model made

--- comedy_hub/db/schema.rb
+++ comedy_hub/db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130911202946) do
+ActiveRecord::Schema.define(version: 20130911203200) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "acts", force: true do |t|
+    t.integer  "comedian_id"
+    t.integer  "show_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "comedians", force: true do |t|
     t.string   "first_name",  null: false
---
+++ comedy_hub/spec/factories/acts.rb
@@ -0,0 +1,8 @@
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :act do
+    comedian_id 1
+    show_id 1
+  end
+end
