58671f75dc01a50699ec221a63b26302133bd317
Alex Phinizy <AlexPhinizy@gmail.com>
2013-09-11 16:41:18 -0400

    first association

--- comedy_hub/app/models/act.rb
+++ comedy_hub/app/models/act.rb
@@ -1,2 +1,4 @@
 class Act < ActiveRecord::Base
+  belongs_to :comedian
+  belongs_to :show
 end
--- comedy_hub/spec/models/act_spec.rb
+++ comedy_hub/spec/models/act_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 
 describe Act do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it {should belong_to(:comedian)}
+  it {should belong_to(:show)}
 end
